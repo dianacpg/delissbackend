9a05c6e46a3448d04d989e26efd889ea94701478
Diana <dianacpgouveia@gmail.com>
2021-04-13 00:23:27 +0100

    database connect

--- server.js
+++ server.js
@@ -59,6 +59,17 @@ app.delete("/favorites/:recipe_id", (req, res) => {
     .catch((err) => res.status(400).json("unable to delete"));
 });
 
+if (process.env.NODE_ENV === "production") {
+  // Exprees will serve up production assets
+  app.use(express.static("client/build"));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(process.env.PORT || 3000, () => {
   console.log(`app is running on port ${process.env.PORT}`);
 });
